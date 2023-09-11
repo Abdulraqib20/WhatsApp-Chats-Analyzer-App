cb5eb0151c3482fba414854f0f8452a0380a370e
raqibcodes <abdulraqibshakir03@gmail.com>
2023-09-10 18:23:19 -0700

    Update chatsanalyzer.py

--- chatsanalyzer.py
+++ chatsanalyzer.py
@@ -56,7 +56,7 @@ video_url = "demo.gif"
 st.image(video_url)
 
 def date_time(s):
-    pattern = r'^\\d{1,2}/\\d{1,2}/\\d{2}, \\d{1,2}:\\d{2}'
+    pattern = r'^\d{1,2}/\d{1,2}/\d{2}, \d{1,2}:\d{2}'
     result = re.match(pattern, s)
     if result:
         return True
@@ -73,18 +73,19 @@ def data_point(line):
 # Create a file upload widget
 uploaded_file = st.file_uploader("Upload your WhatsApp chat .txt file", type=["txt"])
 
-
-# Check if a file was uploaded
 try:
+    
+
+    # Check if a file was uploaded
     if uploaded_file:
         st.success("File successfully uploaded.")
-        
+
         # Process the uploaded file and convert to a Pandas DataFrame
         with st.spinner("Loading📍..."):
             chat_data = uploaded_file.read().decode('utf-8').splitlines()
 
             data = []
-            
+
             message_buffer = []
             date, time, author = None, None, None
             for line in chat_data:
@@ -99,20 +100,20 @@ try:
                     message_buffer.append(message)
                 else:
                     message_buffer.append(line)
-            
+
             # Create a DataFrame
             df = pd.DataFrame(data, columns=['date', 'time', 'member', 'message'])
 
             df['message_type'] = df['message'].apply(lambda x: 'media' if 'attached' in x else 'text')
             df['message_length'] = df['message'].apply(len)
             df['reaction_count'] = df['message'].apply(lambda x: len(re.findall(r'👍|❤️|😂|😢|😮|😡|🎉|👏', x)))
-            df['word_count'] = df['message'].apply(lambda x: len(re.findall(r'\\b\\w+\\b', x)))
-            df['mentions'] = df['message'].apply(lambda x: ", ".join(re.findall(r'@(\\w+)', x)))
+            df['word_count'] = df['message'].apply(lambda x: len(re.findall(r'\b\w+\b', x)))
+            df['mentions'] = df['message'].apply(lambda x: ", ".join(re.findall(r'@(\w+)', x)))
             # df['message'] = df.apply(lambda row: row['message'].replace(f"{row['member']}: ", ""), axis=1)
             # df['message'] = df['message'].str.replace(f"{df['member']}: ", "", regex=True)
             # df['member'] = df['member'].apply(lambda x: re.escape(x))
             # df['message'] = df['message'].str.replace(f"{df['member']}: ", "", regex=True)
-            
+
             # Function to remove member names from messages
             def remove_member_names(df):
                 for index, row in df.iterrows():
@@ -124,7 +125,7 @@ try:
 
             # Apply the function to remove member names
             df = remove_member_names(df)
-            
+
             # get emojis
             def extract_emojis(message):
                 return [emoji.emojize(c) for c in message if emoji.is_emoji(c)]
@@ -132,12 +133,12 @@ try:
             df['emojis'] = df['message'].apply(extract_emojis)
 
             # Remove non-breaking space from the "time" column
-            # df['time'] = df['time'].str.replace('\\u202F', ' ')
+            # df['time'] = df['time'].str.replace('\u202F', ' ')
             # df['time'] = pd.to_datetime(df['time'], format='%I:%M %p').dt.strftime('%I:%M %p')
-            
+
             # Remove non-breaking space from the "time" column
-            df['time'] = df['time'].str.replace('\\u202F', ' ')
-            if df['time'].str.match(r'^\\d{2}:\\d{2}$').all():
+            df['time'] = df['time'].str.replace('\u202F', ' ')
+            if df['time'].str.match(r'^\d{2}:\d{2}$').all():
                 time_format = '%H:%M'  # 24-hour format
             else:
                 time_format = '%I:%M %p'  # AM/PM format
@@ -166,7 +167,7 @@ try:
             # Display the DataFrame if the checkbox is checked
             if show_data:
                 st.dataframe(df)
-                
+
                 # Create a multiselect widget to select columns for unique values
                 selected_columns = st.multiselect("Display Unique Values of Columns", df.columns.tolist())
 
@@ -188,11 +189,11 @@ try:
                 # Set the filename and download button label
                 excel_filename = "whatsapp_chats.xlsx"
                 st.write(f"Exporting to {excel_filename}...")
-                
+
                 # Prepare the Excel data for download
                 excel_data = excel_buffer.getvalue()
                 st.download_button(label="Click here to download the Excel file", data=excel_data, file_name=excel_filename, key="excel_download")
-                
+
     else:
         st.warning("Please upload a WhatsApp chat .txt file.")
 
@@ -227,16 +228,16 @@ try:
     st.subheader("Overall Stats")
 
     def split_count(text):
+
         emoji_list = []
-        data = re.findall(r'[^\\s\\u1f300-\\u1f5ff]', text)
+        data = re.findall(r'[^\s\u1f300-\u1f5ff]', text)
         for word in data:
             if any(char in emoji.distinct_emoji_list(text) for char in word):
                 emoji_list.append(word)
 
         return emoji_list
 
-
-    # Calculate total message
+    # Calculate total messages
     total_messages = df.shape[0]
     # avg messages
     avg_message_length = df['message_length'].mean()
@@ -249,7 +250,7 @@ try:
     emojis = sum(df['emoji'].str.len())
 
     # Calculate URL links
-    URLPATTERN = r'(https?://\\S+)'
+    URLPATTERN = r'(https?://\S+)'
     df['urlcount'] = df.message.apply(lambda x: re.findall(URLPATTERN, x)).str.len()
     links = np.sum(df.urlcount)
 
@@ -272,7 +273,7 @@ try:
     if selected_member:
         st.subheader(f"Stats for {selected_member}")
         member_df = df[df['member'] == selected_member]
-        
+
         # Calculate Messages Sent
         messages_sent = member_df.shape[0]
 
@@ -287,7 +288,7 @@ try:
         emojis_sent = sum(member_df['emoji'].str.len())
 
         # Calculate Links Sent
-        URLPATTERN = r'(https?://\\S+)'
+        URLPATTERN = r'(https?://\S+)'
         member_df['urlcount'] = member_df.message.apply(lambda x: re.findall(URLPATTERN, x)).str.len()
         links_sent = np.sum(member_df.urlcount)
 
@@ -299,6 +300,7 @@ try:
         st.write(f"Links Sent: {links_sent}")
 
     st.title('Visualizations')
+
     # Most Active participants
 
     participant_counts = df['member'].value_counts()
@@ -308,7 +310,7 @@ try:
 
     with st.expander("Most Active Participants", expanded=True):
         show_all_participants = st.checkbox("Show All Participants", value=True)
-        
+
         if not show_all_participants:
             max_participants = st.slider("Max Participants to Show", min_value=1, max_value=len(message_counts), value=len(message_counts))
             message_counts = message_counts.head(max_participants)
@@ -326,7 +328,7 @@ try:
         ).configure_axisX(
             labelAngle=-45
         )
-        
+
         st.altair_chart(chart, use_container_width=True)
 
 
@@ -357,10 +359,10 @@ try:
             color=emojis_only, 
             color_discrete_sequence=colors,
         )
-        
+
         # Customize the chart layout
         fig.update_layout(width=800, height=500, showlegend=True)
-        
+
         # Display the chart using Plotly
         st.plotly_chart(fig)
 
@@ -375,10 +377,10 @@ try:
         top_words = word_freq.most_common(20)
         words = [word[0] for word in top_words]
         counts = [count[1] for count in top_words]
-        
+
         fig = px.bar(pd.DataFrame({'Word': words, 'Count': counts}), x='Word', y='Count')
         fig.update_xaxes(tickangle=0) 
-        
+
         st.plotly_chart(fig)
 
 
@@ -395,13 +397,13 @@ try:
         fig.update_xaxes(showticklabels=False)
         fig.update_yaxes(showticklabels=False)
         st.plotly_chart(fig)
-        
+
     # Most Active Dates
 
     with st.expander('Most Active Dates'):
         activity_by_date = df['date'].value_counts().reset_index()
         activity_by_date.columns = ['Date', 'Activity Count']
-        
+
         fig = px.bar(activity_by_date, x='Date', y='Activity Count', title='Most Active Dates')
         fig.update_traces(marker_color='rgb(63, 72, 204)') 
         fig.update_xaxes(categoryorder='total descending')
@@ -422,29 +424,29 @@ try:
         fig.update_yaxes(title_text='Number of Messages')
         fig.update_layout(width=850, height=550)
         st.plotly_chart(fig)
-        
+
     # Most active hour of the Day
 
     with st.expander('Most Active Hours of the Day'):
         df['hour'] = df['time'].str.split(':', expand=True)[0]
         time_counts = df['hour'].value_counts().reset_index().rename(columns={'index': 'hour', 'hour': 'number of messages'})
         time_counts = time_counts.sort_values(by='hour')
-        
+
         fig = px.bar(time_counts, x='hour', y='number of messages', color='hour',
-                    title='Most Active Times (Hourly)')
+                     title='Most Active Times (Hourly)')
         fig.update_layout(xaxis_title='Hour of the Day', yaxis_title='Number of Messages', showlegend=False)
         st.plotly_chart(fig)
-        
+
     # Most active times
     # with st.expander('Most Active Times'):
     #     time_counts = df['time'].value_counts().head(20).reset_index().rename(columns={'index': 'time', 'time': 'count'})
     #     fig = px.bar(time_counts, x='count', y='time', orientation='h', color='time',
     #                  title='Most Active Times of the Day')
     #     fig.update_layout(xaxis_title='Number of Messages', yaxis_title='Time', showlegend=False)
-        
+
     #     st.plotly_chart(fig)
 
-        
+
     # Most active Days of the Week
     with st.expander('Most Active Days of the Week'):
         df['weekday'] = df['date'].dt.day_name()
@@ -454,14 +456,14 @@ try:
         day_counts = day_counts.sort_values('weekday')
 
         fig = px.bar(day_counts, x='messages', y='weekday', orientation='h', color='weekday',
-                    title='Most Active Days of the Week')
+                     title='Most Active Days of the Week')
         fig.update_layout(xaxis_title='Number of Messages', yaxis_title='Day of the Week', showlegend=False)
         st.plotly_chart(fig)
 
     with st.expander('Most Active Days of the Week'):
         df['weekday'] = pd.to_datetime(df['date']).dt.day_name()
         day_counts = df['weekday'].value_counts().reset_index().rename(columns={'index': 'weekday', 'weekday': 'messages'})
-        
+
         if 'weekday' in day_counts:
             days_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
             day_counts['weekday'] = pd.Categorical(day_counts['weekday'], categories=days_order, ordered=True)
@@ -478,11 +480,11 @@ try:
                 titleFontSize=14,
                 labelFontSize=12
             )
-            
+
             st.altair_chart(chart, use_container_width=True)
         else:
             st.warning("No data available for Most Active Days of the Week.")
-        
+
     # Messages Sent Per Month
     with st.expander('Messages Sent Per Month'):
         df['month'] = pd.to_datetime(df['date']).dt.strftime('%B')
@@ -491,44 +493,45 @@ try:
         months_order = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
         messages_per_month['Month'] = pd.Categorical(messages_per_month['Month'], categories=months_order, ordered=True)
         messages_per_month = messages_per_month.sort_values(by='Month')
-        
+
         fig = px.bar(messages_per_month, x='Month', y='Messages Sent', title='Messages Sent Per Month')
         fig.update_traces(marker_color='rgb(63, 72, 204)')
         fig.update_layout(xaxis_title='Month', yaxis_title='Messages Sent', font=dict(size=14))
-        
+
         st.plotly_chart(fig, use_container_width=True)
-        
+
     # Visualize message count over time
     with st.expander('Messages Over Time'):
         message_count_over_time = df.groupby(['date']).size().reset_index(name='messages')
         fig = px.line(message_count_over_time, x='date', y='messages', title='Messages Over Time')
         st.plotly_chart(fig)
-        
+
     # Visualize message length distribution
     with st.expander('Message Length Distribution'):
         fig = px.histogram(df, x='message_length', title='Message Length Distribution')
         st.plotly_chart(fig)
-        
+
     # Member Activity Over Time
     with st.expander('Member Activity Over Time'):
         member_activity_over_time = df.groupby(['date', 'member']).size().reset_index(name='messages')
         fig = px.line(member_activity_over_time, x='date', y='messages', color='member', title='Member Activity Over Time')
         st.plotly_chart(fig)
-
         
-    # footer
-
-    # line separator
-    st.markdown('<hr style="border: 2px solid #ddd;">', unsafe_allow_html=True)
-
-    # footer text
-    st.markdown(
-        """
-        <div style="text-align: center; padding: 10px;">
-            Developed by <a href="https://github.com/Abdulraqib20" target="_blank">raqibcodes</a>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
 except NameError:
-    st.error("Unable to load the Stats. Please Upload a WhatsApp Chats .txt file.")
+    st.error('Unable to load the Stats. Please Upload a WhatsApp Chats .txt file.')
+
+    
+# footer
+
+# line separator
+st.markdown('<hr style="border: 2px solid #ddd;">', unsafe_allow_html=True)
+
+# footer text
+st.markdown(
+    """
+    <div style="text-align: center; padding: 10px;">
+        Developed by <a href="https://github.com/Abdulraqib20" target="_blank">raqibcodes</a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
